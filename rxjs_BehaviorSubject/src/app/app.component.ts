bfb129b9f1f4dcc8e4e79c9a1a22ef721604d58d
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-27 00:40:38 -0500

    Imp Behaviour Subj

---
+++ rxjs_BehaviorSubject/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rxjs';
+
+  mySubject$;
+
+  ngOnInit() {
+    this.mySubject$ = new BehaviorSubject(200);
+
+    this.mySubject$.subscribe(x => console.log('first subscribe', x));
+
+    this.mySubject$.next(1);
+    this.mySubject$.next(2);
+
+    // subjects are not reusable
+    // this.mySubject$.unsubscribe();
+
+    this.mySubject$.subscribe(x => console.log('second subscribe', x));
+
+    this.mySubject$.next(3);
+  
+  }
+
+  ngOnDestroy(){
+    this.mySubject$.unsubscribe();
+  }
+
+}
