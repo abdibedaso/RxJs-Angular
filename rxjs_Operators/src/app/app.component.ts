41953bc90030b80b305255de1b1cd2957fac9d07
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-27 00:48:11 -0500

    Imp mergeMap

--- rxjs_Operators/src/app/app.component.ts
+++ rxjs_Operators/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { interval } from 'rxjs';
-import { filter, take, map } from  'rxjs/operators';
+import { interval, of } from 'rxjs';
+import { filter, take, map, mergeMap } from  'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +11,27 @@ export class AppComponent {
   title = 'rxjs';
 
   ngOnInit() {
-    const numbers$ = interval(1000);
+    
+    // const numbers$ = interval(1000);
 
-    numbers$
-    .pipe(
-      take(5),
-      map(x => x * 10),
-      filter(x => x > 20)
-    ).subscribe(x => console.log(x));
+    // numbers$
+    // .pipe(
+    //   take(5),
+    //   map(x => x * 10),
+    //   filter(x => x > 20)
+    // ).subscribe(x => console.log(x));
+
+    const _numbers$ = interval(1000);
+    const letters$ = of('a','b','c','d','e');
   
+    letters$.pipe(
+      mergeMap(x => 
+        _numbers$
+        .pipe(
+          take(5),
+          map(i => i + x)
+    ))).subscribe(x => console.log(x));
+
   }
 
   ngOnDestroy(){
