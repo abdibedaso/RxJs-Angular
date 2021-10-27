72bd28244ed695b8653eef90d4109cbd427ffb40
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-27 00:42:00 -0500

    Imp take operator

---
+++ rxjs_Operators/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { filter, take, map } from  'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rxjs';
+
+  ngOnInit() {
+    const numbers$ = interval(1000);
+
+    numbers$
+    .pipe(
+      take(5),
+    ).subscribe(x => console.log(x));
+  
+  }
+
+  ngOnDestroy(){
+  }
+
+}
