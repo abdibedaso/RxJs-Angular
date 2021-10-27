6bb44b7944e4b57f6a5be2c04b8e1421f0d0cf68
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-26 23:45:20 -0500

    Imp observable

--- rxjs_Observer/src/app/app.component.ts
+++ rxjs_Observer/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+
+  observable$;
+
+  ngOnInit() {
+    this.observable$ = new Observable((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+
+    this.observable$.subscribe(
+      value => console.log(value),
+      err => {},
+      () => console.log('this is the end')
+    );
+
+  }
+
+  ngOnDestroy(){
+    this.observable$.unsubscribe();
+  }
 
 }
